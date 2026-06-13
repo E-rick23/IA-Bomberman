1dbf97c755afe3e0aea06820ca26258f5bdf88bc
JezielHonorato <136933391+JezielHonorato@users.noreply.github.com>
2026-06-12 23:18:42 -0300

    Update efeitos.py

--- efeitos.py
+++ efeitos.py
@@ -4,6 +4,7 @@ import config
 
 efeitos_ativos = []
 
+
 class EfeitoVisual:
     def __init__(self, y, x, tipo, frames, tempo_por_frame=100, loop=False):
         self.y = y
@@ -31,19 +32,20 @@ class EfeitoVisual:
                     self.frame_atual = 0
                 else:
                     self.concluido = True
-                    self.frame_atual = len(self.frames) - 1 # Trava no último frame
+                    self.frame_atual = len(self.frames) - 1  # Trava no último frame
 
     def desenhar(self, tela, dicionario_sprites):
         if not self.concluido:
             # Pega o frame atual com base na sequência definida
-            indice_sprite = self.frames[self.frame_atual]
-            sprite = dicionario_sprites[self.tipo][indice_sprite]
+            sprite = self.frames[self.frame_atual]
             pos_x = self.x * config.TILE_SIZE
             pos_y = self.y * config.TILE_SIZE
             tela.blit(sprite, (pos_x, pos_y))
 
+
 def adicionar_efeito(y, x, tipo, frames, tempo_por_frame, loop=False):
     efeitos_ativos.append(EfeitoVisual(y, x, tipo, frames, tempo_por_frame, loop))
+
 
 def atualizar_efeitos(dt):
     global efeitos_ativos
@@ -51,6 +53,7 @@ def atualizar_efeitos(dt):
         efeito.atualizar(dt)
     # Remove os que já terminaram
     efeitos_ativos[:] = [e for e in efeitos_ativos if not e.concluido]
+
 
 def desenhar_efeitos(tela, dicionario_sprites):
     """Percorre a lista de efeitos ativos e desenha cada um na tela."""
